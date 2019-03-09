e065884bcdfd2f3a6651c97f12184719ef92c835
Eyal Toledano <eyaltoledano@me.com>
2019-03-09 22:04:46 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -8,7 +8,7 @@ def consolidate_cart(cart)
     !unique_items.uniq.include?(item.keys.first) ? unique_items << item.keys.first : count += 1
 
     consolidated_cart[item.keys.first] ||= {}
-    consolidated_cart[item.keys.first] ||= {
+    consolidated_cart[item.keys.first] = {
       item.values[0].first[0] => item.values[0].first[1],
       item.values[0].keys[1] => item.values[0].values[1],
       :count => count
